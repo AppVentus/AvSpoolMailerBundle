148d69b15bed862de93705d94b21bc71555122b8
Leny BERNARD <leny@troopers.email>
2018-03-06 16:48:08 +0000

    Apply fixes from StyleCI

--- Mailer/InstantMailer.php
+++ Mailer/InstantMailer.php
@@ -15,38 +15,37 @@ class InstantMailer extends \Swift_Mailer
      * @var string
      */
     protected $entityClass;
+
     public function __construct($em, $transport, $entityClass)
     {
+        $this->em = $em;
 
-    	$this->em = $em;
-
-        $obj = new $entityClass;
+        $obj = new $entityClass();
         if (!$obj instanceof EmailInterface) {
             throw new \InvalidArgumentException("The entity class '{$entityClass}'' does not extend from EmailInterface");
         }
 
         $this->entityClass = $entityClass;
-    	parent::__construct($transport);
+        parent::__construct($transport);
     }
 
-  public function send(\Swift_Mime_Message $message, &$failedRecipients = null)
-  {
+    public function send(\Swift_Mime_Message $message, &$failedRecipients = null)
+    {
         //create a new message object to set content type as html because fosuser mailer create messages in crappy plain text
         $newMessage = \Swift_Message::newInstance()
             ->setSubject($message->getSubject())
             ->setFrom($message->getFrom())
             ->setTo($message->getTo())
-            ->setBody($message->getBody(), 'text/html')
-            ;
-            $newMessage->setReplyTo($message->getReplyTo());
+            ->setBody($message->getBody(), 'text/html');
+        $newMessage->setReplyTo($message->getReplyTo());
 
-        $mail = new $this->entityClass;
+        $mail = new $this->entityClass();
         $mail->setMessage($newMessage);
         $mail->setType('instant');
         $mail->setStatus(EmailInterface::STATUS_COMPLETE);
         $this->em->persist($mail);
         $this->em->flush();
-  	return parent::send($newMessage, $failedRecipients);
-  }
 
+        return parent::send($newMessage, $failedRecipients);
+    }
 }
